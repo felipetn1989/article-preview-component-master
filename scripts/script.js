efe448b5aeefab261d297e18fee8688b13e73c58
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-07 09:24:38 -0300

    Js update

--- scripts/script.js
+++ scripts/script.js
@@ -3,19 +3,7 @@ let button_icon = document.querySelector(".button_icon");
 share.addEventListener("click", overlay);
 
 function overlay() {
-  click.style.display = "flex";
-  share.style.backgroundColor = "var(--Grayish-Blue)";
-  button_icon.setAttribute("src", "images/icon-share-white.png");
-
-  if (window.innerWidth > 1200) {
-    click.removeChild(share_clicked);
+    click.style.display = "flex";
+    share.style.backgroundColor = "var(--Grayish-Blue)";
+    button_icon.setAttribute("src", "images/icon-share-white.png");
   }
-}
-
-share_clicked.addEventListener("click", reclick);
-
-function reclick() {
-  click.style.display = "none";
-  share.style.backgroundColor = "var(--Light-Grayish-Blue)";
-  button_icon.setAttribute("src", "images/icon-share.svg");
-}
