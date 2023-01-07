9f485809343acff7339ed2b2d37f0cecb1bc32e0
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-07 09:50:49 -0300

    finalized active states

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,18 @@ let button_icon = document.querySelector(".button_icon");
 share.addEventListener("click", overlay);
 
 function overlay() {
-    click.style.display = "flex";
-    share.style.backgroundColor = "var(--Grayish-Blue)";
-    button_icon.setAttribute("src", "images/icon-share-white.png");
-  }
+  click.style.display = "flex";
+  share.style.backgroundColor = "var(--Grayish-Blue)";
+  button_icon.setAttribute("src", "images/icon-share-white.png");
+
+  share.addEventListener("click", reclick);
+}
+
+function reclick() {
+  click.style.display = "none";
+  share.style.backgroundColor = "var(--Light-Grayish-Blue)";
+  button_icon.setAttribute("src", "images/icon-share.svg");
+
+  share.removeEventListener("click", reclick)
+  share.addEventListener("click", overlay);
+}
