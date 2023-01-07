fc1cf28e9e5cca00ec8d693752719c1ed93c82fa
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-06 21:15:06 -0300

    adjuested script

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,17 @@
+let button_icon = document.querySelector(".button_icon");
+
 share.addEventListener("click", overlay);
 
 function overlay() {
   click.style.display = "flex";
+  share.style.backgroundColor = "var(--Grayish-Blue)";
+  button_icon.setAttribute("src", "images/icon-share-white.png");
 }
 
 share_clicked.addEventListener("click", reclick);
 
 function reclick() {
   click.style.display = "none";
+  share.style.backgroundColor = "var(--Light-Grayish-Blue)";
+  button_icon.setAttribute("src", "images/icon-share.svg");
 }
