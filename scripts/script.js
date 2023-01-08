1ab71278792a201fff6f95cd3cd8b69bb1d7a7c1
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-08 18:43:31 -0300

    update JS

--- scripts/script.js
+++ scripts/script.js
@@ -3,18 +3,13 @@ let button_icon = document.querySelector(".button_icon");
 share.addEventListener("click", overlay);
 
 function overlay() {
-  click.style.display = "flex";
-  share.style.backgroundColor = "var(--Grayish-Blue)";
-  button_icon.setAttribute("src", "images/icon-share-white.png");
-
-  share.addEventListener("click", reclick);
-}
-
-function reclick() {
-  click.style.display = "none";
-  share.style.backgroundColor = "var(--Light-Grayish-Blue)";
-  button_icon.setAttribute("src", "images/icon-share.svg");
-
-  share.removeEventListener("click", reclick)
-  share.addEventListener("click", overlay);
+  if (click.style.display == 'none') {
+    click.style.display = "flex";
+    share.style.backgroundColor = "var(--Grayish-Blue)";
+    button_icon.setAttribute("src", "images/icon-share-white.png");
+  } else {
+    click.style.display = "none";
+    share.style.backgroundColor = "var(--Light-Grayish-Blue)";
+    button_icon.setAttribute("src", "images/icon-share.svg");
+  }
 }
